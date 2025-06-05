3ca576d98f3e9c3b6c19197be6caf6c093c5e0e8
Майк Немов <mikenemov@MacBook-Air-Majk.local>
2025-06-05 15:54:47 +0300

    Fix Group component and types for build

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,18 @@
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import react from '@vitejs/plugin-react-swc';
-import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig({
-  // Обязательно './' для корректной работы путей к ассетам после билда и деплоя на Vercel
-  base: './',
-
-  css: {
-    preprocessorOptions: {
-      scss: {
-        api: 'modern',
-      },
-    },
-  },
-
+  base: './', // важнейшее, чтобы пути были относительными
   plugins: [
     react(),
     tsconfigPaths(),
-    // Включаем mkcert только если HTTPS (для локальной разработки с SSL)
-    process.env.HTTPS && mkcert(),
   ],
-
   build: {
     target: 'esnext',
   },
-
-  publicDir: './public',
-
+  publicDir: 'public',
   server: {
-    host: true, // Доступно в локальной сети
+    host: true,
   },
 });
